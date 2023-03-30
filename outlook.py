26a44cf23f6195b3dd4d9ebcc5c35b0117ade76a
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-03-30 17:59:20 +0900

    ファイルを指定して添付するメールを作成できるようにした

--- outlook.py
+++ outlook.py
@@ -1,5 +1,14 @@
+import pdb  # デバッグ用
+# pdb.set_trace()
 import win32com.client
 import mail_secret
+from tkinter import filedialog, Tk
+import sys
+import datetime
+import re  # 正規表現
+import os
+import shutil
+from datetime import datetime
 
 outlook = win32com.client.Dispatch("Outlook.Application")
 mail = outlook.CreateItem(0)  # 0でメールオブジェクトの生成
@@ -8,9 +17,37 @@ mail = outlook.CreateItem(0)  # 0でメールオブジェクトの生成
 mail.to = mail_secret.mail_to
 mail.cc = mail_secret.mail_cc
 
-inputDay = input('日付を入力: ')
-mail.subject = inputDay + '管理部配送予定'
-mail.bodyFormat = 1  # 1:テキストとして送信
-mail.body = mail_secret.mail_body(inputDay)
+# 日付を取得しバリデーション
+input_day = input('日付を入力(例 12/3): ')
+DAY_PATTERN = '\d{1,2}/\d{1,2}'
+repatter = re.compile(DAY_PATTERN)
+result = repatter.match(input_day)
 
-mail.display(True)
+if not(result):
+    print('[ERROR] 例のように日付を入力してください')
+    exit(0)
+
+date_obj = datetime.strptime(input_day, '%m/%d')
+
+print(result.group())
+
+# 添付するPDFのファイルパスを取得
+root = Tk()
+root.withdraw()
+src_file = filedialog.askopenfilename(filetypes=[("PDFファイル", "*.pdf")], title="スキャンしたpdfファイルを選択")
+
+if src_file == "":
+    print("ファイルが選択されませんでした。")
+    sys.exit()
+
+# PDFファイルを実行中のpythonファイルと同じディレクトリにコピー
+dst_file = os.path.join(os.path.dirname(__file__), "管理部配送予定" + date_obj.strftime('%m%d') + '.pdf')
+shutil.copy2(src_file, dst_file)
+
+mail.Attachments.Add(dst_file)
+
+mail.subject = input_day + '管理部配送予定'
+mail.bodyFormat = 1  # 1:テキストとして送信
+mail.body = mail_secret.mail_body(input_day)
+
+mail.display(False)
